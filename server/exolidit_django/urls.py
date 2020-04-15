e59000acc6d753fba1f5c36e2bbb9a655edcc85e
Noam <noams@epd-medical.com>
2020-04-15 19:21:28 +0300

    much simpler clearer hello world by django's official doc

--- server/exolidit_django/urls.py
+++ server/exolidit_django/urls.py
@@ -16,14 +16,32 @@ Including another URLconf
 from django.urls import include, path
 from rest_framework import routers
 from server.quickstart import views
+from django.urls import include
+from django.contrib import admin
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
+
+
+# def foo(arg):
+#     from django.http import HttpResponse
+#     import datetime
+#     now = datetime.datetime.now()
+#     html = "<html><body>It is now %s.</body></html>" % now
+#     print(f"foo, {arg}")
+#     return HttpResponse(html)
+
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # path('', include(router.urls)),
+    # path('aaa', foo),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('', views.index, name='index'),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
 ]
+
