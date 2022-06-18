2fde112d94704fe12a592fe081d7274dbc2f6e49
NoamWindowsLaptop <noam.s@uveye.com>
2022-06-18 14:54:05 +0300

    initial commit once again

--- Main.py
+++ Main.py
@@ -1,8 +1,19 @@
 import os
+from tika import parser
+
+
+
+
+
+def main():
+    pdf_folder_path = r"C:\Users\noam.s\Desktop\important\money\cal"
+    pdf_name = r"2020_10_.pdf"
+    pdf_path = os.path.join(pdf_folder_path, pdf_name)
+
+    raw = parser.from_file(pdf_path)
+    print(raw["content"])
+    pass
 
 
 if __name__ == "__main__":
-    def main():
-        print(os.getcwd())
-
     main()
